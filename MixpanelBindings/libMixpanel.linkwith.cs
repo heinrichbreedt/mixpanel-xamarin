864a04b466ebb563d3df3005fc0bbd50fad0b118
softlion <softlion@softlion.com>
2015-09-03 17:35:16 +0200

    reset linkwith

--- MixpanelBindings/libMixpanel.linkwith.cs
+++ MixpanelBindings/libMixpanel.linkwith.cs
@@ -1,7 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libMixPanel.a", LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
-	Frameworks = "Foundation, CoreTelephony, UIKit, SystemConfiguration", 
-	WeakFrameworks = "AdSupport",  LinkerFlags="-licucore", 
-	ForceLoad = true)]
+[assembly: LinkWith ("libMixpanel.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
