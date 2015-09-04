41ef51f0788d2d45e87c25471e1605dbb2631d26
softlion <softlion@softlion.com>
2015-09-04 11:43:25 +0200

    works on ios, in test on android

--- MixpanelBindings/libMixpanel.linkwith.cs
+++ MixpanelBindings/libMixpanel.linkwith.cs
@@ -1,5 +1,9 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libMixpanel.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, 
+//[assembly: LinkWith ("libMixpanel.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, 
+//Removed Simulator64, it does not work with the xamarin version used (error on link in the test project)
+//Removed armv7s, it is not required for appstore submission and does not provide improvements
+
+[assembly: LinkWith ("libMixpanel.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.Arm64, SmartLink = true, 
     Frameworks = "Security SystemConfiguration QuartzCore CFNetwork CoreTelephony", LinkerFlags = "-licucore", ForceLoad = true)]
